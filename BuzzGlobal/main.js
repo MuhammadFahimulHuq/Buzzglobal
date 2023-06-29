c9a25071bb9aa87b95353db427d325eff44dc65a
unknown <fahimul.huq85@gmail.com>
2023-06-29 19:25:18 +0600

    upto portfolio

---
+++ BuzzGlobal/main.js
@@ -0,0 +1,10 @@
+
+
+var copyAds = document.querySelector('.slider').cloneNode(true);
+document.querySelector('.sponsored-ad').appendChild(copyAds);
+
+var copyBands = document.querySelector('.slider-right').cloneNode(true);
+document.querySelector('.bands-ads').appendChild(copyBands);
+
+
+
